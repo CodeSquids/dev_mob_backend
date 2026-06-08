5ff8b0821534e6497b6a985855127357433f86bc
NafNafra <nafafyandrianaivo@gmail.com>
2026-06-08 15:45:09 +0300

    fix: port forgotten fix

--- src/config/init.ts
+++ src/config/init.ts
@@ -10,6 +10,7 @@ export const sequelize = new Sequelize(
   process.env.DB_PASSWORD!,
   {
     host: process.env.DB_HOSTNAME!,
+    port: Number(process.env.DB_PORT),
     dialect: (process.env.DB_DIALECT!) as 'mysql',
   }
 )
